a143816cba39661ddea1d7069572353e75f48a72
Joseph Garcia <prosif@gmail.com>
2023-04-30 11:40:44 -0700

    more

--- index.js
+++ index.js
@@ -170,71 +170,79 @@ const getCertStatus = (username, token) => new Promise((resolve, reject) => {
     req.end();
 });
 
-if (getConfigValue('HTTPS_ENABLED', false)) {
-    console.log('sdfdsfdsfdsfdsf');
-    doLogin().then(({username, token}) => {
-        getCertStatus(username, token).then((_certStatus) => {
-            console.log('here is cert status');
-            const certStatus = JSON.parse(_certStatus);
-            console.log(certStatus);
-            if (!certStatus.certFound || !certStatus.certExpiration || certStatus.certExpiration <= Date.now()) {
-                requestCert(username, token).then(keyBundle => {
-                    console.log("KEY BUDNLE");
-                    console.log(keyBundle);
-                    const keyBuf = Buffer.from(keyBundle, 'base64');
-                    const keyStream = bufToStream(keyBuf);
-                    const unzip = unzipper.Extract({ path: baseDir });
-                    keyStream.pipe(unzip);
+const certPathArgs = process.argv.filter(a => a.startsWith('--cert-path=')).map(a => a.replace('--cert-path=', ''));
+const certPathArg = certPathArgs && certPathArgs.length > 0 ? certPathArgs[0] : null;
 
-                    unzip.on('close', () => {
-                        console.log('finished unzipping! waiting for cert...');
+console.log("CERT PATH ARG");
+console.log(certPathArg);
 
-                        const timeoutTime = Date.now() + (60 * 3 * 1000);
-                        let timeWaited = 0;
-                        const checker = setInterval(() => {
-                            console.log('Checking for cert...');
-                            if (Date.now() >= timeoutTime) {
-                                console.error('Timed out waiting for cert');
-                                clearInterval(checker);
-                            } else {
-                                console.log('need to check...');
-                                getCertStatus(username, token).then((_currentStatus) => {
-                                    console.log('cert info!');
-                                    const currentStatus = JSON.parse(_currentStatus);
-                                    if (currentStatus) {
-                                        if (currentStatus.certData) {
-                                            console.log('got cert!');
-                                            clearInterval(checker);
-                                            const certBuf = Buffer.from(currentStatus.certData, 'base64');
-                                            fs.writeFileSync(`${baseDir}/hg-certs/homegames.cert`, certBuf);
-                                            server(`${baseDir}/hg-certs`);
-                                        }
-                                    }
-                                });
-                            }
+server(certPathArg);
 
-                        }, 20 * 1000);
-                    });
-                });
-            } else {
-                // start server
-                if (certStatus.certFound) {
-                    server(`${baseDir}/hg-certs`);
-                } else {
-	            server();
-                }
-            }
-        });
-    });
-} else {
-
-//const certPath = getConfigValue('CERT_PATH', null);
-//const keyPath = getConfigValue('KEY_PATH', null);
-
-//if (certPath && keyPath) {
-//	console.log('secure server start');
-//	server({ certPath, keyPath });
+//if (false && getConfigValue('HTTPS_ENABLED', false)) {
+//    console.log('sdfdsfdsfdsfdsf');
+//    doLogin().then(({username, token}) => {
+//        getCertStatus(username, token).then((_certStatus) => {
+//            console.log('here is cert status');
+//            const certStatus = JSON.parse(_certStatus);
+//            console.log(certStatus);
+//            if (!certStatus.certFound || !certStatus.certExpiration || certStatus.certExpiration <= Date.now()) {
+//                requestCert(username, token).then(keyBundle => {
+//                    console.log("KEY BUDNLE");
+//                    console.log(keyBundle);
+//                    const keyBuf = Buffer.from(keyBundle, 'base64');
+//                    const keyStream = bufToStream(keyBuf);
+//                    const unzip = unzipper.Extract({ path: baseDir });
+//                    keyStream.pipe(unzip);
+//
+//                    unzip.on('close', () => {
+//                        console.log('finished unzipping! waiting for cert...');
+//
+//                        const timeoutTime = Date.now() + (60 * 3 * 1000);
+//                        let timeWaited = 0;
+//                        const checker = setInterval(() => {
+//                            console.log('Checking for cert...');
+//                            if (Date.now() >= timeoutTime) {
+//                                console.error('Timed out waiting for cert');
+//                                clearInterval(checker);
+//                            } else {
+//                                console.log('need to check...');
+//                                getCertStatus(username, token).then((_currentStatus) => {
+//                                    console.log('cert info!');
+//                                    const currentStatus = JSON.parse(_currentStatus);
+//                                    if (currentStatus) {
+//                                        if (currentStatus.certData) {
+//                                            console.log('got cert!');
+//                                            clearInterval(checker);
+//                                            const certBuf = Buffer.from(currentStatus.certData, 'base64');
+//                                            fs.writeFileSync(`${baseDir}/hg-certs/homegames.cert`, certBuf);
+//                                            server(`${baseDir}/hg-certs`);
+//                                        }
+//                                    }
+//                                });
+//                            }
+//
+//                        }, 20 * 1000);
+//                    });
+//                });
+//            } else {
+//                // start server
+//                if (certStatus.certFound) {
+//                    server(`${baseDir}/hg-certs`);
+//                } else {
+//	            server();
+//                }
+//            }
+//        });
+//    });
 //} else {
-	log.info('insecure server start');
-}
+//
+////const certPath = getConfigValue('CERT_PATH', null);
+////const keyPath = getConfigValue('KEY_PATH', null);
+//
+////if (certPath && keyPath) {
+////	console.log('secure server start');
+////	server({ certPath, keyPath });
+////} else {
+//	log.info('insecure server start');
+//}
 
